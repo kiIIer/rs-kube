ee970ee34b73f4ee3ccb5a9ae42beccaf451051b
killer <michael.molchanov.2004@gmail.com>
2023-12-28 20:32:08 +0100

    maybe worker is done, idk

--- server/src/bin/server.rs
+++ server/src/bin/server.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use anyhow::Result;
+use server::server::run_server;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    run_server()
+}
--- server/src/bin/worker.rs
+++ server/src/bin/worker.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use anyhow::Result;
+use server::worker::run_worker;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    run_worker().await?;
+    Ok(())
+}
